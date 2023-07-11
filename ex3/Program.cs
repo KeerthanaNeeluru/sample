fe77c6bed0b8690322e1ebe0321c7998842d33ce
keerthana <keerthananeeluru2001@gmail.com>
2023-07-11 11:17:57 +0530

    fille adding

---
+++ ex3/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string cCode;
+            string lang,choice;
+
+            do {
+                Console.WriteLine("Enter Country code");
+                cCode = Console.ReadLine().ToUpper();
+
+                switch (cCode)
+                {
+                    case "UK":
+                    case "US":
+                        {
+                            lang = "English";
+                            break;
+                        }
+                    case "AF":
+                        {
+                            lang = "Dari,Pashto";
+                            break;
+                        }
+                    case "IND":
+                        {
+                            lang = "Hindi,english,tamil";
+                            break;
+                        }
+                    case "UAE":
+                    case "OM":
+                    case "KSA":
+                    case "EGYPT":
+                        {
+                            lang = "Arabic";
+                            break;
+                        }
+                    default:
+                        {
+                            Console.WriteLine($"sorry !!! your country {cCode} is not in our database");
+                            lang = "Not Found !!!!!!!";
+                            break;
+                        }
+
+                }
+                Console.WriteLine("Country Code : " + cCode + "\nLanguage(s) " + lang);
+                Console.WriteLine("If you want to continue press y else press n");
+                choice = Console.ReadLine().ToUpper();
+            } while (choice=="Y");
+                Console.ReadKey();
+        }
+    }
+}
